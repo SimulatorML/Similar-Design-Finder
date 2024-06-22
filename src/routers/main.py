aba2b7e2437e8861cc15163a3e550e6423a17904
Sokolov Fedor <sokol_f4@mail.ru>
2024-06-22 17:01:10 +0300

    Minor fix

--- src/routers/main.py
+++ src/routers/main.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from src.config import settings
+from src.repositories import CollectionRepository, DocsRepository
 from src.routers.schemas import FinderResult, FindRequest
 from src.services.finder import FinderService
 
@@ -14,6 +15,8 @@ async def get_finder() -> FinderService:
         collection_name=settings.COLLECTION_NAME,
         model_name=settings.EMBEDDING_MODEL_NAME,
         model_cache_dir=settings.MODEL_CACHE_DIR,
+        collection_repository=CollectionRepository(),
+        docs_repository=DocsRepository(),
     )
 
 
