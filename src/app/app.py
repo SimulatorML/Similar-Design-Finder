06212d9bb11dc1d8c32a13fafb509dcbf4d5b03a
Sokolov Fedor <sokol_f4@mail.ru>
2024-07-30 16:54:46 +0300

    Add Prometheus instrumentator

--- src/app/app.py
+++ src/app/app.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Response, status
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from src.config import settings
 from src.routers.main import router as finder_router
 
 app = FastAPI()
 app.include_router(finder_router, prefix=settings.API_VERSION_STR, tags=["Documents"])
+
+Instrumentator().instrument(app).expose(app)
 
 
 @app.get("/health")
